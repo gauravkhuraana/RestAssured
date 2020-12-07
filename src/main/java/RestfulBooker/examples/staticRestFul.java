fd88fd9971b63e5864b5b5f0a51ecc6aab08e8f8
Gaurav Khurana <gaurav.khurana@microsoft.com>
2020-12-07 19:09:19 +0530

    Added the changes till static import

---
+++ src/main/java/RestfulBooker/examples/staticRestFul.java
@@ -0,0 +1,31 @@
+package RestfulBooker.examples;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+public class staticRestFul {
+
+    @Test
+    public void BDDStyleWithStaticImport()
+
+    {
+        // given
+        given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                // when
+       .when()
+                .get("/booking")
+       .then()
+                .statusCode(200)
+                .statusLine("HTTP/1.1 200 OK")
+                .statusLine("HTTP/1.1 200 OK")
+               // .body("bookingid",hasSize(10))
+                .body("bookingid[3]",equalTo(1));
+
+
+    }
+}
